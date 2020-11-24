8ad0cf095e690c4bca1ba6e58f9f75a97fa51a18
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-11-24 09:38:46 +0700

    Upload Project

---
+++ src/main/java/id/rachman/garislurus/GarisLurus.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.rachman.garislurus;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rachman Aldiansyah
+ * KELAS    : IF-1
+ * NIM      : 10119038
+ * Deskripsi Program : Program ini berisi interface GarisLurus
+ * 
+ */
+
+public interface GarisLurus {
+    public int hitungGradien();
+}
